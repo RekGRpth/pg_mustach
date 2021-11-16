de688112c75b910db1d90adebd03a9ac34206351
RekGRpth <rekgrpth@gmail.com>
2021-11-16 13:30:02 +0500

    test

--- sql/comments.sql
+++ sql/comments.sql
@@ -7,15 +7,28 @@
 \set ON_ERROR_STOP true
 BEGIN;
 CREATE EXTENSION pg_mustach;
-SELECT 1, 'Inline', replace(mustach('{}', E'12345{{! Comment Block! }}67890'), E'\n', '\n');
-SELECT 2, 'Multiline', replace(mustach('{}', E'12345{{!\n  This is a\n  multi-line comment...\n}}67890\n'), E'\n', '\n');
-SELECT 3, 'Standalone', replace(mustach('{}', E'Begin.\n{{! Comment Block! }}\nEnd.\n'), E'\n', '\n');
-SELECT 4, 'Indented Standalone', replace(mustach('{}', E'Begin.\n  {{! Indented Comment Block! }}\nEnd.\n'), E'\n', '\n');
-SELECT 5, 'Standalone Line Endings', replace(replace(mustach('{}', E'|\r\n{{! Standalone Comment }}\r\n|'), E'\r', '\r'), E'\n', '\n');
-SELECT 6, 'Standalone Without Previous Line', replace(mustach('{}', E'  {{! I''m Still Standalone }}\n!'), E'\n', '\n');
-SELECT 7, 'Standalone Without Newline', replace(mustach('{}', E'!\n  {{! I''m Still Standalone }}'), E'\n', '\n');
-SELECT 8, 'Multiline Standalone', replace(mustach('{}', E'Begin.\n{{!\nSomething''s going on here...\n}}\nEnd.\n'), E'\n', '\n');
-SELECT 9, 'Indented Multiline Standalone', replace(mustach('{}', E'Begin.\n  {{!\n    Something''s going on here...\n  }}\nEnd.\n'), E'\n', '\n');
-SELECT 10, 'Indented Inline', replace(mustach('{}', E'  12 {{! 34 }}\n'), E'\n', '\n');
-SELECT 11, 'Surrounding Whitespace', replace(mustach('{}', E'12345 {{! Comment Block! }} 67890'), E'\n', '\n');
+COPY (
+SELECT 1, 'Inline', mustach('{}', E'12345{{! Comment Block! }}67890')
+UNION
+SELECT 2, 'Multiline', mustach('{}', E'12345{{!\n  This is a\n  multi-line comment...\n}}67890\n')
+UNION
+SELECT 3, 'Standalone', mustach('{}', E'Begin.\n{{! Comment Block! }}\nEnd.\n')
+UNION
+SELECT 4, 'Indented Standalone', mustach('{}', E'Begin.\n  {{! Indented Comment Block! }}\nEnd.\n')
+UNION
+SELECT 5, 'Standalone Line Endings', mustach('{}', E'|\r\n{{! Standalone Comment }}\r\n|')
+UNION
+SELECT 6, 'Standalone Without Previous Line', mustach('{}', E'  {{! I''m Still Standalone }}\n!')
+UNION
+SELECT 7, 'Standalone Without Newline', mustach('{}', E'!\n  {{! I''m Still Standalone }}')
+UNION
+SELECT 8, 'Multiline Standalone', mustach('{}', E'Begin.\n{{!\nSomething''s going on here...\n}}\nEnd.\n')
+UNION
+SELECT 9, 'Indented Multiline Standalone', mustach('{}', E'Begin.\n  {{!\n    Something''s going on here...\n  }}\nEnd.\n')
+UNION
+SELECT 10, 'Indented Inline', mustach('{}', E'  12 {{! 34 }}\n')
+UNION
+SELECT 11, 'Surrounding Whitespace', mustach('{}', E'12345 {{! Comment Block! }} 67890')
+ORDER BY 1
+) TO stdout;
 ROLLBACK;
--- sql/delimiters.sql
+++ sql/delimiters.sql
@@ -7,18 +7,34 @@
 \set ON_ERROR_STOP true
 BEGIN;
 CREATE EXTENSION pg_mustach;
-SELECT 1, 'Pair Behavior', mustach('{"text": "Hey!"}', '{{=<% %>=}}(<%text%>)');
-SELECT 2, 'Special Characters', mustach('{"text": "It worked!"}', '({{=[ ]=}}[text])');
-SELECT 3, 'Special Characters', mustach('{"section": true, "data": "I got interpolated."}', '[\n{{#section}}\n  {{data}}\n  |data|\n{{/section}}\n\n{{=| |=}}\n|#section|\n  {{data}}\n  |data|\n|/section|\n]\n');
-SELECT 4, 'Inverted Sections', mustach('{"section": false, "data": "I got interpolated."}', '[\n{{^section}}\n  {{data}}\n  |data|\n{{/section}}\n\n{{=| |=}}\n|^section|\n  {{data}}\n  |data|\n|/section|\n]\n');
-SELECT 5, 'Partial Inheritence', mustach('{"value": "yes", "include": ".{{value}}."}', '[ {{>include}} ]\n{{=| |=}}\n[ |>include| ]\n');
-SELECT 6, 'Post-Partial Behavior', mustach('{"value": "yes", "include": ".{{value}}. {{=| |=}} .|value|."}', '[ {{>include}} ]\n[ .{{value}}.  .|value|. ]\n');
-SELECT 7, 'Surrounding Whitespace', mustach('{}', '| {{=@ @=}} |');
-SELECT 8, 'Outlying Whitespace (Inline)', mustach('{}', ' | {{=@ @=}}\n');
-SELECT 9, 'Standalone Tag', mustach('{}', 'Begin.\n  {{=@ @=}}\nEnd.\n');
-SELECT 10, 'Indented Standalone Tag', mustach('{}', 'Begin.\n  {{=@ @=}}\nEnd.\n');
-SELECT 11, 'Standalone Line Endings', mustach('{}', '|\r\n{{=@ @=}}\r\n|');
-SELECT 12, 'Standalone Without Previous Line', mustach('{}', '  {{=@ @=}}\n=');
-SELECT 13, 'Standalone Without Newline', mustach('{}', '=\n  {{=@ @=}}');
-SELECT 14, 'Pair with Padding', mustach('{}', '|{{=@   @=}}|');
+COPY (
+SELECT 1, 'Pair Behavior', mustach('{"text": "Hey!"}', E'{{=<% %>=}}(<%text%>)')
+UNION
+SELECT 2, 'Special Characters', mustach('{"text": "It worked!"}', E'({{=[ ]=}}[text])')
+UNION
+SELECT 3, 'Special Characters', mustach('{"section": true, "data": "I got interpolated."}', E'[\n{{#section}}\n  {{data}}\n  |data|\n{{/section}}\n\n{{=| |=}}\n|#section|\n  {{data}}\n  |data|\n|/section|\n]\n')
+UNION
+SELECT 4, 'Inverted Sections', mustach('{"section": false, "data": "I got interpolated."}', E'[\n{{^section}}\n  {{data}}\n  |data|\n{{/section}}\n\n{{=| |=}}\n|^section|\n  {{data}}\n  |data|\n|/section|\n]\n')
+UNION
+SELECT 5, 'Partial Inheritence', mustach('{"value": "yes", "include": ".{{value}}."}', E'[ {{>include}} ]\n{{=| |=}}\n[ |>include| ]\n')
+UNION
+SELECT 6, 'Post-Partial Behavior', mustach('{"value": "yes", "include": ".{{value}}. {{=| |=}} .|value|."}', E'[ {{>include}} ]\n[ .{{value}}.  .|value|. ]\n')
+UNION
+SELECT 7, 'Surrounding Whitespace', mustach('{}', E'| {{=@ @=}} |')
+UNION
+SELECT 8, 'Outlying Whitespace (Inline)', mustach('{}', E' | {{=@ @=}}\n')
+UNION
+SELECT 9, 'Standalone Tag', mustach('{}', E'Begin.\n  {{=@ @=}}\nEnd.\n')
+UNION
+SELECT 10, 'Indented Standalone Tag', mustach('{}', E'Begin.\n  {{=@ @=}}\nEnd.\n')
+UNION
+SELECT 11, 'Standalone Line Endings', mustach('{}', E'|\r\n{{=@ @=}}\r\n|')
+UNION
+SELECT 12, 'Standalone Without Previous Line', mustach('{}', E'  {{=@ @=}}\n=')
+UNION
+SELECT 13, 'Standalone Without Newline', mustach('{}', E'=\n  {{=@ @=}}')
+UNION
+SELECT 14, 'Pair with Padding', mustach('{}', E'|{{=@   @=}}|')
+ORDER BY 1
+) TO stdout;
 ROLLBACK;
