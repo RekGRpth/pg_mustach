395dda9c61685dcb538b820f438864ba695ac484
RekGRpth <rekgrpth@gmail.com>
2021-11-17 09:30:35 +0500

    test

--- sql/handlebars.sql
+++ sql/handlebars.sql
@@ -26,8 +26,8 @@ SELECT 4, 'depth-1', mustach('{"names": [{"name": "Moe"}, {"name": "Larry"}, {"n
 SELECT 5, 'depth-2', mustach('{"names": [{"bat": "foo", "name": ["Moe"]}, {"bat": "foo", "name": ["Larry"]}, {"bat": "foo", "name": ["Curly"]}, {"bat": "foo", "name": ["Shemp"]}], "foo": "bar"}', '{{#names}}{{#name}}{{bat}}{{foo}}{{/name}}{{/names}}');
 SELECT 6, 'object', mustach('{"person": {"name": "Larry", "age": 45}}', '{{#person}}{{name}}{{age}}{{/person}}');
 SELECT 7, 'object-mustach', mustach('{"person": {"name": "Larry", "age": 45}}', '{{#person}}{{name}}{{age}}{{/person}}');
-SELECT 8, 'partial', mustach('{"peeps": [{"name": "Moe", "count": 15}, {"name": "Larry", "count": 5}, {"name": "Curly", "count": 1}]}', '{{#peeps}}{{>variables}}{{/peeps}}');
-SELECT 9, 'partial-recursion', mustach('{"name": "1", "kids": [{"name": "1.1", "kids": [{"name": "1.1.1", "kids": []}]}]}', '{{name}}{{#kids}}{{>recursion}}{{/kids}}');
+SELECT 8, 'partial', mustach('{"peeps": [{"name": "Moe", "count": 15}, {"name": "Larry", "count": 5}, {"name": "Curly", "count": 1}], "variables": "Hello {{name}}! You have {{count}} new messages."}', '{{#peeps}}{{>variables}}{{/peeps}}');
+SELECT 9, 'partial-recursion', mustach('{"name": "1", "kids": [{"name": "1.1", "kids": [{"name": "1.1.1", "kids": []}]}], "recursion": "{{name}}{{#kids}}{{>recursion}}{{/kids}}"}', '{{name}}{{#kids}}{{>recursion}}{{/kids}}');
 SELECT 10, 'paths', mustach('{"person": {"name": {"bar": {"baz": "Larry"}}, "age": 45}}', '{{person.name.bar.baz}}{{person.age}}{{person.foo}}{{animal.age}}');
 SELECT 11, 'string', mustach('{}', 'Hello world');
 SELECT 12, 'variables', mustach('{"name": "Mick", "count": 30}', 'Hello {{name}}! You have {{count}} new messages.');
