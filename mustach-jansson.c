efeacecc986563d6b561288b347f065c4a86d3db
RekGRpth <rekgrpth@gmail.com>
2022-01-27 12:54:16 +0500

    fix

--- mustach-jansson.c
+++ mustach-jansson.c
@@ -4,12 +4,14 @@ int mustach_process_jansson(const char *template, size_t length, const char *buf
 
 #if __has_include("mustach/mustach-jansson.h")
 #include "mustach/mustach-jansson.h"
+#include <string.h>
 
 int mustach_process_jansson(const char *template, size_t length, const char *buffer, size_t buflen, int flags, FILE *file, char **err) {
     int rc = MUSTACH_ERROR_USER(1);
+    static char text[JSON_ERROR_TEXT_LENGTH];
     json_error_t error;
     json_t *root;
-    if (!(root = json_loadb(buffer, buflen, JSON_DECODE_ANY, &error))) { *err = (char *)error.text; goto ret; }
+    if (!(root = json_loadb(buffer, buflen, JSON_DECODE_ANY, &error))) { *err = strncpy(text, error.text, JSON_ERROR_TEXT_LENGTH); goto ret; }
     rc = mustach_jansson_file(template, length, root, flags, file);
     json_decref(root);
 ret:
